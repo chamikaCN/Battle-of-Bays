fd6bdbf71022bfee90817a42c7cc6f0f2850bf7e
chamikaCN <chamikanandasiri97@gmail.com>
2020-07-24 20:15:07 +0530

    healthbars added

---
+++ Assets/Scripts/BillBoard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    Transform cam;
+
+    void Start()
+    {
+        cam = GameController.instance.getCamera().transform;
+    }
+    void Update()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    Slider slider;
+    public Gradient gradient;
+    int maxHealth;
+    public Image fill;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void setMaxHealth(int health)
+    {
+        maxHealth = health;
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void changeHealth(int newHealth)
+    {
+        slider.value = newHealth;
+        fill.color = gradient.Evaluate(slider.value / 5);
+    }
+
+}
