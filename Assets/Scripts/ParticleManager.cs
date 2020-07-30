2cbae587e403a1ae118f613483f586110b7848a4
chamikaCN <chamikanandasiri97@gmail.com>
2020-07-30 23:02:00 +0530

    particle manager to controls lifetime

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    public float lifeTime;
+    void Start()
+    {
+        StartCoroutine(endExplosion());
+    }
+
+    IEnumerator endExplosion()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(this.gameObject);
+    }
+
+}
