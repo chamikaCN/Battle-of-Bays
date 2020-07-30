79af892fd8974886a7449d6daaa81780d08169f3
chamikaCN <chamikanandasiri97@gmail.com>
2020-07-30 22:57:46 +0530

    mesh collider regeneration added

---
+++ Assets/Scripts/meshColliderGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshColliderGenerator : MonoBehaviour
+{
+    #region Singleton
+    public static meshColliderGenerator instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("many cameracontollers");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+    MeshCollider mc ;
+    void Start()
+    {
+        mc = GetComponent<MeshCollider>();
+    }
+
+    public void setMesh(Mesh m){
+        mc.sharedMesh = m;
+    }
+}
