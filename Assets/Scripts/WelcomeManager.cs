2a786e23c0857e93b77401f6861e9f835f012a28
chamikaCN <chamikanandasiri97@gmail.com>
2020-08-06 16:56:07 +0530

    welcome scene added

---
+++ Assets/Scripts/WelcomeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WelcomeManager : MonoBehaviour
+{
+    public void LoadGame(string team)
+    {
+        PlayerPrefs.SetString("Team", team);
+        SceneManager.LoadScene("DevScene");
+    }
+}
